0fd5cc99432878d8881aff4c83d77c56932432a1
Ivan3628 <ivan35@ptt.rs>
2020-09-13 11:34:08 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,36 @@
-document.querySelector("form").addEventListener("submit", searchProperties);
-let inputVal = document.querySelector("input").value;
+import cities from "./cities";
+import { ui } from "./ui";
 
-function searchProperties() {
+document.querySelector("form").addEventListener("submit", searchProperties);
+
+const getReprString = (city) => `${city.city}, ${city.code}`;
+
+//Integrate auto complete library
+
+var my_autoComplete = new autoComplete({
+  selector: 'input[name="search"]',
+  minChars: 3,
+  source: function (term, suggest) {
+    term = term.toLowerCase();
+    var choices = cities;
+    var matches = [];
+    for (let i = 0; i < choices.length; i++)
+      if (~getReprString(choices[i]).toLowerCase().indexOf(term))
+        matches.push(getReprString(choices[i]));
+    suggest(matches);
+  },
+});
+
+//Search properties
+
+function searchProperties(e) {
+  e.preventDefault();
+  const [city, stateCode] = document
+    .querySelector('input[name="search"]')
+    .value.split(",")
+    .map((_) => _.trim());
   fetch(
-    "https://realtor.p.rapidapi.com/properties/v2/list-for-sale?sort=relevance&city=New%20York%20City&limit=200&offset=0&state_code=NY",
+    `https://realtor.p.rapidapi.com/properties/v2/list-for-sale?sort=relevance&city=${city}&limit=50&offset=0&state_code=${stateCode}`,
     {
       method: "GET",
       headers: {
@@ -17,6 +44,7 @@ function searchProperties() {
     })
     .then((response) => {
       console.log(response);
+      ui.propertiesForSale(response);
     })
     .catch((err) => {
       console.log(err);
